ec07a0a8f2dd6e09a18549ea46bce98c18d7f610
df275 <df275@EVIL>
2025-11-19 20:26:55 +0500

    Разработанное главное окно. Чернышков

--- TimeLord_Чернышков/Properties/AssemblyInfo.cs
+++ TimeLord_Чернышков/Properties/AssemblyInfo.cs
@@ -8,11 +8,11 @@ using System.Windows;
 // набор атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
 [assembly: AssemblyTitle("TimeLord_Чернышков")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Данное приложение позволяет измерять интервалы времени с точностью до долей секунды.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TimeLord_Чернышков")]
-[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyCopyright("Чернышков К.К. ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
